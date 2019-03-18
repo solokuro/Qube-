a994b745cc0eec9490487f56cda6524c923fb30f
fox <i>
2019-03-18 20:37:39 +0900

    修正

--- type 1 Qube/Assets/script/goal.cs
+++ type 1 Qube/Assets/script/goal.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class goal : MonoBehaviour {
 
     public static bool goalflag;
-    private string anserword;
+    [SerializeField] private string anserword;
     // Use this for initialization
     void Start () {
 

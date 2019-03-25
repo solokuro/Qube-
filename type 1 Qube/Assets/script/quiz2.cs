4f0b128165ff422d4df7723f1aa43d745bafd80e
fox <i>
2019-03-25 16:03:34 +0900

    閉じようにした

--- type 1 Qube/Assets/script/quiz2.cs
+++ type 1 Qube/Assets/script/quiz2.cs
@@ -5,31 +5,54 @@ using UnityEngine;
 public class quiz2 : MonoBehaviour
 {
 
-	
-	public static bool moveflag2;
-	private Animator animator2;
-	
+    private Ray ray;
+    private RaycastHit hit;
+    private GameObject select;
+    public static bool moveflag2;
+    private Animator animator2,closeanimator;
+
     // Start is called before the first frame update
     void Start()
     {
-		
-		moveflag2 = false;
-		animator2 = GetComponent<Animator>();
+
+        moveflag2 = false;
+        animator2 = GetComponent<Animator>();
+        closeanimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.R))
-		{
-			Quiz2();
-		}
+        if (Input.GetMouseButtonDown(1))
+        {
+            Quiz2();
+        }
     }
 
-	void Quiz2()
-	{
-		
-		moveflag2 = true;
-		this.animator2.SetTrigger("open");
-	}
+    void Quiz2()
+    {
+        select = null;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 11))
+        {
+            select = hit.collider.gameObject;
+            switch (select.name)
+            {
+              case "openbutton":
+              moveflag2 = true;
+              this.animator2.SetTrigger("open");
+              this.closeanimator.SetTrigger("close");
+              break;
+            }
+
+            
+        
+        }
+
+
+    } 
+
+
 }
+
+
--- type 1 Qube/Assets/script/quizstart.cs
+++ type 1 Qube/Assets/script/quizstart.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class quizstart : MonoBehaviour
 {
-
+    private Ray ray;
+    private RaycastHit hit;
+    private GameObject select;
     public static bool moveflag;
-    private Animator animator3;
+    private Animator animator3, closeanimator;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,22 +17,41 @@ public class quizstart : MonoBehaviour
 
         moveflag = false;
         animator3 = GetComponent<Animator>();
+        closeanimator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      if(Input.GetKey(KeyCode.A))
+        if (Input.GetMouseButtonDown(1))
         {
             Quizstart();
+
         }
+
 
     }
 
     void Quizstart()
     {
-        moveflag = true;
-        this.animator3.SetTrigger("open");
+        select = null;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 10))
+        {
+            select = hit.collider.gameObject;
+            switch (select.name)
+            {
+                case "openbutton":
+                    moveflag = true;
+                    this.animator3.SetTrigger("open");
+                    this.closeanimator.SetTrigger("close");
+                    break;
+            }
+
+
+        }
     }
+
 
 }
